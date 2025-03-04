88fcbaa03d299cd34f5ecab273a0dab25b91882f
sunil patil <sunilpatil12000@gmail.com>
2025-03-04 13:45:19 +0530

    config file cros add

--- config/config.js
+++ config/config.js
@@ -7,5 +7,5 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
   JWT_EXPIRE: process.env.JWT_EXPIRE || '30d',
   JWT_COOKIE_EXPIRE: process.env.JWT_COOKIE_EXPIRE || 30,
-  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173'
+  CLIENT_URL: process.env.CLIENT_URL || 'https://techblogi.netlify.app'
 };
